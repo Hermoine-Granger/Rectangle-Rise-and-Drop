9bb8811754e69c8fe7efd63d677c26775b9e47d0
Himani Sharma <hsharma316@gmail.com>
2021-02-04 15:54:48 +0530

    Update Rectangles_Optimized_1.py

--- Rectangles_Optimized_1.py
+++ Rectangles_Optimized_1.py
@@ -1,30 +1,43 @@
+'''
+This code stores x points-height as key value pairs.
+So for loop has to travel only through keys and not through all continuous points.
+Also keys are sorted . So when we set max height to given range, we just have to get hashed index and trverse through all keys smaller than  x2.
+'''
+
 n=int(input("Enter total number of coordinate Tuples:"))
 recs=[]
 mx_range=0
 min_range=99999999999
+height={} # Dictionary contains height at x point
+
 
 print("Enter tuples in form x1 x2 h")
 for i in range(n):
-	x=tuple(map(int,input().split()))
-	recs.append(x)
-	mx_range=max(mx_range,x[1])
-	min_range=min(min_range,x[0])
+    x=tuple(map(int,input().split()))
+    recs.append(x)
+    height[x[0]]=0
+    height[x[1]]=0
 	
-height={i:0 for i in range(min_range,mx_range+1)}
+keys_list=list(height.keys())
+keys_list.sort() #contains sorted x points keys
+index_list={keys_list[i]:i for i in range(len(keys_list))} #Dictionary containing index of x points in keys_list
+
 
 for each in recs:
     x1,x2,h=each[0],each[1],each[2]
-    for i in range(x1,x2):
-        height[i]=max(height[i],h)
-    height[x2]=max(0,height[x2])
+    strt=index_list[x1]
+    end=index_list[x2]
+    for i in range(strt,end):
+        height[keys_list[i]]=max(height[keys_list[i]],h)
+    height[keys_list[i]]=max(0,height[keys_list[i]])
 	
 prev_height=-1
-for i in range(min_range,mx_range+1):
-    if height[i]==prev_height:
+for key in keys_list:
+    if height[key]==prev_height:
         pass
     else:
-        prev_height=height[i]
-        print("(",i," ",height[i],")")
+        prev_height=height[key]
+        print("(",key," ",height[key],")")
         
 		
 
